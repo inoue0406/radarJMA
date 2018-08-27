67034da36b95505da6f44c6222466de9326cbc2d
Tsuyoshi <inoue0406@gmail.com>
2018-08-28 00:56:23 +0900

    Added Prediction by optical-flow method

---
+++ src/main_OFpredict_jma.py
@@ -0,0 +1,63 @@
+import torch 
+import torchvision
+import numpy as np
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+import pandas as pd
+import h5py
+import os
+import sys
+import json
+import time
+
+import pdb
+
+from jma_pytorch_dataset import *
+from test_OFpred import *
+from utils import Logger
+from opts import parse_opts
+
+# Prediction by Optical Flow
+# and semi-Lagrangian advection
+
+if __name__ == '__main__':
+   
+    # parse options
+    opt = parse_opts()
+    print(opt)
+    # create result dir
+    if not os.path.exists(opt.result_path):
+        os.mkdir(opt.result_path)
+    
+    with open(os.path.join(opt.result_path, 'opts.json'), 'w') as opt_file:
+        json.dump(vars(opt), opt_file)
+
+    # generic log file
+    logfile = open(os.path.join(opt.result_path, 'log_run.txt'),'w')
+    logfile.write('Start time:'+time.ctime()+'\n')
+    tstart = time.time()
+        
+    # loading datasets (we only need testation data)
+    test_dataset = JMARadarDataset(root_dir=opt.data_path,
+                                    csv_file=opt.train_path,
+                                    tdim_use=opt.tdim_use,
+                                    transform=None)
+    test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                               batch_size=opt.batch_size,
+                                               shuffle=False)
+    # Prep logger
+    test_logger = Logger(
+        os.path.join(opt.result_path, 'test.log'),
+        ['loss', 'RMSE', 'CSI', 'FAR', 'POD', 'Cor'])
+
+    # Test for Optical Flow Forecast
+    loss_fn = torch.nn.MSELoss()
+    test_OFpred(test_loader,loss_fn,
+                test_logger,opt)
+
+    # output elapsed time
+    logfile.write('End time: '+time.ctime()+'\n')
+    tend = time.time()
+    tdiff = float(tend-tstart)/3600.0
+    logfile.write('Elapsed time[hours]: %f \n' % tdiff)
---
+++ src/test_OFpred.py
@@ -0,0 +1,145 @@
+import torch 
+import torchvision
+import numpy as np
+import torch.utils.data as data
+import torchvision.transforms as transforms
+
+# for optical flow
+from scipy import interpolate
+import cv2
+
+from jma_pytorch_dataset import *
+from convolution_lstm_mod import *
+from utils import AverageMeter, Logger
+from criteria_precip import *
+
+# testing for Optical Flow forecast
+
+def OF_predictor(data_torch):
+    # data : tensor with dimension of [tsize,width,height]
+    # define xy grid
+    data = data_torch.numpy()
+    tsize = data.shape[0]
+    xsize = data.shape[1]
+    ysize = data.shape[2]
+    data_nxt = np.zeros((tsize,xsize,ysize))
+    xgrd = np.arange(0,xsize)
+    ygrd = np.arange(0,ysize)
+    X, Y = np.meshgrid(xgrd,ygrd)
+    #-------------------------------------
+    # Step1 : Get flow vector by optical flow
+    flow_all = np.zeros((tsize,xsize,ysize,2))
+    ofsize = 6
+    for i in range((tsize-1-ofsize),(tsize-1)):
+        #print('calc OF between ',i,' and ',i+1)
+        flow_all[i-5,:,:] = cv2.calcOpticalFlowFarneback(data[i,:,:],data[i+1,:,:],None,
+                                                         pyr_scale=0.5,levels=2,winsize=10,iterations=3,
+                                                         poly_n=5,poly_sigma=1.1,flags=False)
+    # take median as representative OF
+    medflow = np.median(flow_all,axis=0)
+    #-------------------------------------
+    # Step2 : Time-evolution by semi-Lagrangian method
+    # x(n) = x(n-1) + dt*v
+    # -> x(n-1) = x(n) - dt*v
+    VX = medflow[:,:,0]
+    VY = medflow[:,:,1]
+    # prep interp func
+    R = data[tsize-1,:,:] # use last picture as initial condition
+    itp_rain = interpolate.RectBivariateSpline(xgrd, ygrd, R)
+    Rp = data[tsize-2,:,:] # previous step for change ratio calculation
+    itp_rain_p = interpolate.RectBivariateSpline(xgrd, ygrd, Rp)
+
+    # calc change ratio along with time evolution
+    dt = 1.0
+    xp1 = X - dt*VX
+    yp1 = Y - dt*VY
+    xp2 = X - 2.0*dt*VX
+    yp2 = Y - 2.0*dt*VY
+    # apply interpolation
+    Rp1 = itp_rain.ev(xp1,yp1)
+    Rp2 = itp_rain_p.ev(xp2,yp2)
+    flg_r = Rp2 > 0.5 # rain existence flag
+    eps = 0.001
+    # if no rain, set ratio=1.0
+    ratio = flg_r*Rp1/(Rp2+eps) + (1.0-flg_r)*1.0
+    #import pdb; pdb.set_trace()
+    for it in range(tsize):
+        dt = 1.0*(it+1)
+        xprev = X - dt*VX
+        yprev = Y - dt*VY
+        # apply interpolation
+        data_nxt[it,:,:] = itp_rain.ev(xprev,yprev)*np.power(ratio,(it+1))
+    return(torch.from_numpy(data_nxt))
+            
+def test_OFpred(test_loader,loss_fn,test_logger,opt):
+    print('Test for Optical Flow forecast')
+    
+    losses = AverageMeter()
+    
+    # initialize
+    SumSE_all = np.empty((0,opt.tdim_use),float)
+    hit_all = np.empty((0,opt.tdim_use),float)
+    miss_all = np.empty((0,opt.tdim_use),float)
+    falarm_all = np.empty((0,opt.tdim_use),float)
+    m_xy_all = np.empty((0,opt.tdim_use),float)
+    m_xx_all = np.empty((0,opt.tdim_use),float)
+    m_yy_all = np.empty((0,opt.tdim_use),float)
+
+    for i_batch, sample_batched in enumerate(test_loader):
+        input = Variable(sample_batched['past']).cpu()
+        target = Variable(sample_batched['future']).cpu()
+
+        print('batch:',i_batch,'\n')
+        # Prediction by Persistence
+        output = target.clone()
+        for n in range(input.data.shape[0]):
+            output.data[n,:,0,:,:] = OF_predictor(input.data[n,:,0,:,:])
+            
+        loss = loss_fn(output, target)
+
+        # for logging
+        losses.update(loss.data[0], input.size(0))
+        
+        # apply evaluation metric
+        SumSE,hit,miss,falarm,m_xy,m_xx,m_yy = StatRainfall(target.data.cpu().numpy()*201.0,
+                                                            output.data.cpu().numpy()*201.0,
+                                                            th=0.5)
+        SumSE_all = np.append(SumSE_all,SumSE,axis=0)
+        hit_all = np.append(hit_all,hit,axis=0)
+        miss_all = np.append(miss_all,miss,axis=0)
+        falarm_all = np.append(falarm_all,falarm,axis=0)
+        m_xy_all = np.append(m_xy_all,m_xy,axis=0)
+        m_xx_all = np.append(m_xx_all,m_xx,axis=0)
+        m_yy_all = np.append(m_yy_all,m_yy,axis=0)
+        
+        #if (i_batch+1) % 100 == 0:
+        if (i_batch+1) % 1 == 0:
+            print ('Testing, Iter [%d/%d] Loss: %.4e' 
+                   %(i_batch+1, len(test_loader.dataset)//test_loader.batch_size, loss.data[0]))
+    # logging for averaged loss
+    RMSE,CSI,FAR,POD,Cor = MetricRainfall(SumSE_all,hit_all,miss_all,falarm_all,
+                                          m_xy_all,m_xx_all,m_yy_all,axis=None)
+    test_logger.log({
+        'loss': losses.avg,
+        'RMSE': RMSE,
+        'CSI': CSI,
+        'FAR': FAR,
+        'POD': POD,
+        'Cor': Cor})
+    # logging for loss by time
+    RMSE,CSI,FAR,POD,Cor = MetricRainfall(SumSE_all,hit_all,miss_all,falarm_all,
+                                          m_xy_all,m_xx_all,m_yy_all,axis=(0))
+    # save evaluated metric as csv file
+    tpred = (np.arange(opt.tdim_use)+1.0)*5.0 # in minutes
+    # import pdb; pdb.set_trace()
+    df = pd.DataFrame({'tpred_min':tpred,
+                       'RMSE':RMSE,
+                       'CSI':CSI,
+                       'FAR':FAR,
+                       'POD':POD,
+                       'Cor':Cor})
+    df.to_csv(os.path.join(opt.result_path, 'test_evaluation_predtime.csv'))
+
+    
+
+    
