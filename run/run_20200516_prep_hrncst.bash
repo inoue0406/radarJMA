2adafdd40952999b729237f9622cbc608a8526f9
Tsuyoshi Inoue <inoue0406@gmail.com>
2020-05-19 22:22:07 +0900

    small tweaks

---
+++ run/run_20200516_prep_hrncst.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#python ../src_prep/prep_hrncst_JMA.py 2017-04 >& log-04 &
+#python ../src_prep/prep_hrncst_JMA.py 2017-07 >& log-07 &
+#python ../src_prep/prep_hrncst_JMA.py 2017-08 >& log-08 &
+#python ../src_prep/prep_hrncst_JMA.py 2017-09 >& log-09 &
+python ../src_prep/prep_hrncst_JMA.py 2017-10 >& log-10 &
+#python ../src_prep/prep_hrncst_JMA.py 2017-11 >& log-11 &
+python ../src_prep/prep_hrncst_JMA.py 2017-12 >& log-12 &
+
---
+++ run/run_20200519_prep_hrncst.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#python ../src_prep/prep_hrncst_JMA.py 2017-04 >& log-04 &
+#python ../src_prep/prep_hrncst_JMA.py 2017-07 >& log-07 &
+#python ../src_prep/prep_hrncst_JMA.py 2017-08 >& log-08 &
+#python ../src_prep/prep_hrncst_JMA.py 2017-09 >& log-09 &
+python ../src_prep/prep_hrncst_JMA.py 2017-10-29 >& log-10-29 &
+python ../src_prep/prep_hrncst_JMA.py 2017-10-30 >& log-10-30 &
+python ../src_prep/prep_hrncst_JMA.py 2017-10-31 >& log-10-31 &
+
+python ../src_prep/prep_hrncst_JMA.py 2017-12-29 >& log-12-29 &
+python ../src_prep/prep_hrncst_JMA.py 2017-12-30 >& log-12-30 &
+python ../src_prep/prep_hrncst_JMA.py 2017-12-31 >& log-12-31 &
+#python ../src_prep/prep_hrncst_JMA.py 2017-11 >& log-11 &
+
+
